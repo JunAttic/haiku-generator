879a6f35cd1541681fb727c402ff55dd7a782160
JunAttic <junattic@outlook.it>
2025-07-15 16:11:53 +0200

    added ai

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateHaiku(event) {
-  event.preventDefault();
-
+function displayHaiku(response) {
   new Typewriter("#haiku", {
-    strings: "Text of the haiku",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateHaiku(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instruction-input");
+  let apiKey = "b0o49ce9af4193689e78b7100091727t";
+  let prompt = `Generate a Japanese Haiku about ${instructionsInput.value}`;
+  let context =
+    "you are an Haiku expert,your goal is to generate 2 lines haiku, separate each line with a <br /> ,make sure to follow user instruction.The last word of the haiku MUST be inside a <strong> element";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayHaiku);
 }
 
 let haikuFormElement = document.querySelector("#haiku-generator");
